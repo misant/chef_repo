efc933a4a657061ea75dca118d88a134ba859c3c
root <root@ip-172-31-22-207.eu-central-1.compute.internal>
2016-02-22 20:02:22 +0000

    second commit

---
+++ cookbooks/phpapp/attributes/default.rb
@@ -0,0 +1,5 @@
+default["phpapp"]["database"] = "phpapp"
+default["phpapp"]["db_username"] = "phpapp"
+default["phpapp"]["path"] = "/etc/apache2/htdocs"
+default['phpapp']['server_name']= "phpapp"
+
